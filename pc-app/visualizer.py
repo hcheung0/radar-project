e2abaa180d6c183898bba90be47b038c508840cf
MuchSoup <158533514+MuchSoup@users.noreply.github.com>
2026-09-12 16:30:10 -0400

    Add polar grid with range lines. Refactored shared center calculation

--- pc-app/visualizer.py
+++ pc-app/visualizer.py
@@ -1,6 +1,8 @@
 import pygame
 import math
 
+PIXELS_PER_CM = 550/400
+
 def init_display():
     pygame.init()
     screen = pygame.display.set_mode((800, 600))
@@ -9,10 +11,22 @@ def init_display():
 def draw_frame(screen, reading):
     screen.fill((0, 0, 0))
 
+    draw_grid(screen)
+
     if reading is not None:
-        pygame.draw.circle(screen, (0, 255, 0), polar_to_pixel(reading[0], reading[1], screen.get_width()//2, screen.get_height() - 30), 3)
+        pygame.draw.circle(screen, (0, 255, 0), polar_to_pixel(reading[0], reading[1], screen.get_width()//2, screen.get_height()-10), 3)
 
     pygame.display.flip()
+
+def draw_grid(screen):
+    center_x = screen.get_width()//2
+    center_y = screen.get_height() - 10
+
+    for distance_cm in range(50, 401, 50):
+        r = distance_cm * PIXELS_PER_CM
+        rectangle = pygame.Rect(center_x-r, center_y-r, 2*r, 2*r)
+        pygame.draw.arc(screen, (105, 105, 105), rectangle, 0, math.pi, 2)
+
 
 
 def handle_events():
@@ -24,8 +38,8 @@ def handle_events():
 
 def polar_to_pixel(angle, distance, center_x, center_y):
     angle_rad = math.radians(angle)
-    x_offset = distance * math.cos(angle_rad)
-    y_offset = -1 * distance * math.sin(angle_rad)
+    x_offset = distance * math.cos(angle_rad) * PIXELS_PER_CM
+    y_offset = -1 * distance * math.sin(angle_rad) * PIXELS_PER_CM
 
     x = center_x + x_offset
     y = center_y + y_offset
